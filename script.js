b0293c19d7a3d8f5f416ba623e1add70b9e0e708
IsraaDeveloper <israadeveloper@gmail.com>
2025-03-08 19:47:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -295,7 +295,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const botMessageElement = addMessage('...', 'bot');
 
         try {
-            const response = await fetch('http://panel.imicid.my.id:25565/chat', {
+            const response = await fetch('https://apikey-hci1.vercel.app/chat', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ message: userMessage })
